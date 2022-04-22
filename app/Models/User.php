a083d660fa54adf1d1503f9017a9613c03aebe75
Betul Kacira <karicabetul@gmail.com>
2022-04-22 11:12:38 +0300

    satırlar eklendi.

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,12 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
         'email',
+        'type',
         'password',
+        'level',
+
     ];
 
     /**
