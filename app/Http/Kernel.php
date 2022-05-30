4f974f90c7660d08370c75366c230edf6cd00d27
kcrBetül <kacirabetul@gmail.com>
2022-05-30 17:31:38 +0300

    İsadmini middleware  icine tanımlamışım  hata verdi sadece classın içine tanımlamam gerekiyormuş onu düzelttik. "Bunu beraber yaptık diye hatırlıyorum."

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\IsAdmin::class,
+
     ];
 
     /**
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'IsAdmin' => \App\Http\Middleware\IsAdmin::class,
+        'isLogin' => \App\Http\Middleware\isLogin::class,
     ];
 }
